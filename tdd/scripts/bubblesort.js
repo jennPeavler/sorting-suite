c7c419912603b8d28e5ddaf59e36fa627fc13ea8
Jennifer Peavler <jenn.peavler@gmail.com>
2017-03-29 13:05:55 -0600

    set up test suite.  a few passing test for makeArray function.  A few passing test for bubblesort function

--- tdd/scripts/bubblesort.js
+++ tdd/scripts/bubblesort.js
@@ -1,31 +1,31 @@
 
 
 let makeArray = () => {
-  let array = [];
-  let length = 5000;
+  let unsortedArray = [];
+  let numElements = 10;
   let max  = 100;
   let min = -100;
-    for (let i = 0; i < length; i++) {
-      array.push(Math.floor(Math.random() * (max - (min))) + (min))
+    for (let i = 0; i < numElements; i++) {
+      unsortedArray.push(Math.floor(Math.random() * (max - (min))) + (min))
     }
-  return array;
+
+  console.log(unsortedArray);
+  return unsortedArray;
 }
 
-let bubblesort = array => {
-  for (let i = 0; i < array.length - 1; i++) {
-    console.log(array);
-    if(array[i] > array[i+1]) {
-      let temp = array[i+1];
-      array[i+1] = array[i]
-      array[i] = temp;
+let bubblesort = sortedArray => {
+  for (let i = 0; i < sortedArray.length - 1; i++) {
+    if(sortedArray[i] > sortedArray[i+1]) {
+      let temp = sortedArray[i+1];
+      sortedArray[i+1] = sortedArray[i]
+      sortedArray[i] = temp;
       i-=2;
     }
   }
-  return array;
+  return sortedArray;
 }
 
-var array = makeArray();
-bubblesort(array);
+let unsortedArray = makeArray();
+bubblesort(unsortedArray);
 
-export default bubblesort;
-export default makeArray;
+module.exports = bubblesort, makeArray;
--- tdd/tests/bubblesort-test.js
+++ tdd/tests/bubblesort-test.js
@@ -1,4 +1,5 @@
 import { assert } from 'chai';
+import { expect } from 'chai';
 import bubblesort from '../scripts/bubblesort'
 import makeArray from '../scripts/bubblesort'
 
@@ -6,10 +7,28 @@ describe('the function makeArray', () => {
   it('should be a function', () => {
     assert.isFunction(makeArray)
   })
+
+  it.skip('should make an array', () => {
+    bubblesort([1, 2, 3])
+    expect(Array.isArray(makeArray()));
+  })
+
+  it.skip('should make an array with the desired length', () => {
+    let unsortedArray = makeArray();
+
+    // console.log(unsortedArray);
+    expect(unsortedArray).to.have.lengthOf(10);
+  })
 })
 
 describe('the function bubblesort', () => {
   it('should be a function', () => {
     assert.isFunction(bubblesort)
+  })
+
+  it('should be able to return a sorted array of numbers', () => {
+    let sortedArray = bubblesort([666, 13, -333, 99, -3.14])
+
+    assert.deepEqual(sortedArray, [-333, -3.14, 13, 99, 666])
   })
 })

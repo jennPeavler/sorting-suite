4bbd1abbfaa08785d1b4cd14842aa6b1debc2fdf
Jennifer Peavler <jenn.peavler@gmail.com>
2017-03-29 14:08:17 -0600

    makeArray and bubblesort function tests passing

--- tdd/scripts/makeArray.js
+++ tdd/scripts/makeArray.js
@@ -6,8 +6,6 @@ let makeArray = () => {
     for (let i = 0; i < numElements; i++) {
       unsortedArray.push(Math.floor(Math.random() * (max - (min))) + (min))
     }
-
-  console.log(unsortedArray);
   return unsortedArray;
 }
 
--- tdd/tests/bubblesort-test.js
+++ tdd/tests/bubblesort-test.js
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import { expect } from 'chai';
+// import { expect } from 'chai';
 import bubblesort from '../scripts/bubblesort'
 
 describe('the function bubblesort', () => {
@@ -12,4 +12,25 @@ describe('the function bubblesort', () => {
 
     assert.deepEqual(sortedArray, [-333, -3.14, 13, 99, 666])
   })
+
+  it('should be able to return a sorted array of letters', () => {
+    let sortedArray = bubblesort(['z', 'w', 'a', 'b', 'j', 'p'])
+
+    assert.deepEqual(sortedArray, ['a', 'b', 'j', 'p', 'w', 'z'])
+  })
+
+  it('should be able to sort really big arrays', () => {
+    let unsortedArray = [];
+    let numElements = 22222;
+    let max  = 100;
+    let min = -100;
+
+    for (let i = 0; i < numElements; i++) {
+      unsortedArray.push(Math.floor(Math.random() * (max - (min))) + (min))
+    }
+    let sortedArray = unsortedArray.sort();
+
+    assert.deepEqual(bubblesort(unsortedArray), sortedArray);
+  })
+
 })

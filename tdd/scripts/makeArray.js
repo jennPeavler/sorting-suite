c4b9977366ddcd5bf1eebcb2106fe62af378f8b2
Jennifer Peavler <jenn.peavler@gmail.com>
2017-03-30 09:15:26 -0600

    clean up

--- tdd/scripts/makeArray.js
+++ tdd/scripts/makeArray.js
@@ -3,9 +3,10 @@ let makeArray = () => {
   let numElements = 10;
   let max  = 100;
   let min = -100;
-    for (let i = 0; i < numElements; i++) {
-      unsortedArray.push(Math.floor(Math.random() * (max - (min))) + (min))
-    }
+  
+  for (let i = 0; i < numElements; i++) {
+    unsortedArray.push(Math.floor(Math.random() * (max - (min))) + (min))
+  }
   return unsortedArray;
 }
 
